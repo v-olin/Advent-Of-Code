062bfc0998de33b65abbe3b5d46b798e1c92852a
Victor Olin <victorvi@student.chalmers.se>
2021-12-12 00:13:04 +0100

    Comments for days 9 and 10

--- 2021/Day 10/sol.hs
+++ 2021/Day 10/sol.hs
@@ -19,10 +19,12 @@ main = do vals <- getVals "input.txt"
           print (sum $ part1 series)
           print (part2 series)
 
+-- Read file to a 2d array of chars as strings
 getVals :: FilePath -> IO [[String]]
 getVals path = do contents <- readFile path
                   return (map (splitOn "") (lines contents))
 
+-- The corresponding closing bracket of an opening bracket
 inverse :: String -> String
 inverse s = case s of "(" -> ")"
                       "[" -> "]"
@@ -30,6 +32,7 @@ inverse s = case s of "(" -> ")"
                       "<" -> ">"
                       _ -> ""
 
+-- Points for each bracket depending on part1 or part2
 points :: String -> Int
 points s = case s of ")" -> 3
                      "]" -> 57
@@ -41,9 +44,12 @@ points s = case s of ")" -> 3
                      "<" -> 4
                      _ -> 0
 
+-- If bracket is an opening bracket
 isOpen :: String -> Bool
 isOpen s = s `elem` ["(","[","{","<"]
 
+-- Goes through all brackets until first mis-match happens, then
+-- return the current state of the stack and the mis-matched char
 runLine :: Stack String -> [String] -> (Stack String, String)
 runLine stack [] = (stack, "")
 runLine stack (s:ss)
@@ -55,6 +61,7 @@ runLine stack (s:ss)
         top = fromMaybe "!" (stackPeek stack)
         pop = fst (fromJust (stackPop stack))
 
+-- Convert stack to list
 popAll :: Stack String -> [String]
 popAll stack
     | isNothing popd = []
@@ -62,9 +69,12 @@ popAll stack
     where
         popd = stackPop stack
 
+-- Get all mis-matched characters and convert to points
 part1 :: [[String]] -> [Int]
 part1 = map (points . snd . runLine stackNew)
 
+-- Get all incomplete stacks, map the stacks to a list of points,
+-- fold them and find the middle score
 part2 :: [[String]] -> Int
 part2 ss = sort scores !! (length scores `div` 2)
     where
--- 2021/Day 9/sol.hs
+++ 2021/Day 9/sol.hs
@@ -18,42 +18,52 @@ main = do vals <- getVals "input.txt"
           print (part1 mtrx)
           print (part2 mtrx)
 
+-- Read file to a 2d string array
 getVals :: FilePath -> IO [[String]]
 getVals path = do contents <- readFile path
                   return (map (splitOn "") (lines contents))
 
+-- Filter away whitespace and convert the rest to ints
 filterWhtSpc :: [String] -> [Int]
 filterWhtSpc = map (read::String->Int) . filter (/= "")
 
+-- Get indices for points up, down, left and right of given position,
+-- then filter away OOB indices
 offsets :: Matrix Int -> (Int, Int) -> [(Int, Int)]
 offsets m (x,y) = filter (\(a,b) -> not (a < 1 || a > h || b < 1 || b > l)) ps
     where
         ps = map (\(a,b) -> (a+x,b+y)) [(0,-1),(1,0),(0,1),(-1,0)]
         (l,h) = (ncols m, nrows m)
 
+-- An element can be part of basin if lower than 9
 basin :: Matrix Int -> (Int, Int) -> Bool
 basin m (x,y) = getElem x y m /= 9
 
+-- Check if all points around are larger than element in matrix at p
 checkLow :: Matrix Int -> (Int, Int) -> Bool
 checkLow m p = all (\(x,y) -> n < getElem x y m) (offsets m p)
     where
         n = uncurry getElem p m
 
+-- Fins all lows in matrix
 findLows :: Matrix Int -> [(Int, Int)]
 findLows m = filter (checkLow m) ps
     where
         (l, h) = (ncols m, nrows m)
         ps = concatMap (\x' -> map (x',) [1..l]) [1..h]
 
+-- Apply arithmetic according to part 1 to all low points
 part1 :: Matrix Int -> Int
 part1 m = sum $ map ((+ 1) . (\(x,y) -> getElem x y m)) (findLows m)
 
+-- Recursive floodfill algorithm to find size of a basin
 fill :: Matrix Int -> [(Int, Int)] -> [(Int, Int)] -> Int
 fill _ [] vstd = length (nub vstd)
 fill m (p:ps) vstd = fill m ps' (vstd ++ [p])
     where
         ps' = filter (basin m) (ps ++ (offsets m p \\ vstd))
 
+-- Take the product of the 3 largest basins
 part2 :: Matrix Int -> Int
 part2 m = product (take 3 (reverse (sort (map (\p -> fill m [p] []) lows))))
     where
