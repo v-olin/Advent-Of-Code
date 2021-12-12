c9346404d77a1f203ec51c1c3f8b031b248f4bec
Victor Olin <victorvi@student.chalmers.se>
2021-12-12 11:32:27 +0100

    Day 8 both parts finished!

--- 2021/Day 8/sol.hs
+++ 2021/Day 8/sol.hs
@@ -1,78 +1,45 @@
 import System.IO()
 import Data.List
-import Data.List.Split
-
-main :: IO()
-main = do ls <- getVals "test.txt"
-          let parsed = parseCombos ls
-          print ("Part 1: " ++ show (part1 parsed))
-        --   print occurrences
-        --   print stdSums
-        --   print (sumFromStr "abcde")
-        
-          print ("Part 2: " ++ show (part2 parsed))
+import Data.Map (Map, (!), fromList)
+import qualified Data.Map as Map
+import Data.List.Split (splitOn)
 
 type Combo = ([String], [String])
-type Signal = [(Int, String)]
 
-p1digits :: [Int]
-p1digits = [2, 4, 3, 7]
-
-occurrences :: [(Char, Int)]
-occurrences = zip ['a'..'g'] [8,6,8,7,4,9,7]
-
-stdSums :: [(Int, Int)]
-stdSums = zip [0..9] [42,17,34,39,30,37,41,25,49,45]
-
-segments :: [String]
-segments = splitOn " " s
-    where
-        s = "abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg"
-
-findOccs :: String -> Char -> [(Char, Int)]
-findOccs s c = zip ['a'..'g'] ns
-    where
-        ns = map (length . (filter (\c' -> c' == c) s)) ['a'..'g']
-
--- Filter away whitespaces or newlines
-noWhtSpc :: String -> Bool
-noWhtSpc s = if s == "" then False else True
+main :: IO()
+main = do ls <- getVals "input.txt"
+          let combos = map parseCombo ls
+          print (part1 combos)
+          print (part2 combos)
 
 getVals :: FilePath -> IO [String]
 getVals path = do contents <- readFile path
                   return (lines contents)
 
+stdMap :: Map Int Int
+stdMap = fromList (zip [42,17,34,39,30,37,41,25,49,45] [0..9])
+
 parseCombo :: String -> Combo
-parseCombo s = (s' !! 0, s' !! 1)
+parseCombo s = (head s', s' !! 1)
     where
-        s' = map (filter noWhtSpc) (map (splitOn " ") (splitOn "|" s))
-
-parseCombos :: [String] -> [Combo]
-parseCombos ss = map parseCombo ss
-
-checkLen :: [String] -> Int
-checkLen ss = length $ intersect ss' p1digits
-    where
-        ss' = map (\s -> length s) ss
+        s' = map (filter (/="") . splitOn " ") (splitOn "|" s)
 
 part1 :: [Combo] -> Int
-part1 cs = foldl (+) 0 $ map (\(a,b) -> checkLen b) cs
+part1 cs = length $ concatMap (filter (`elem` [2,4,3,7]) . map length . snd) cs
 
-sumFromStr :: [(Char, Int)] -> String -> Int
-sumFromStr occs s = sum $ map (\c -> (snd ((fromChar c) !! 0))) s
+makeMap :: [String] -> Map Char Int
+makeMap ss = fromList (zip ['a'..'g'] occs)
     where
-        fromChar c = filter (\oc -> if (fst oc) == c then True else False) occs
-
-digitFromSum :: [(Int, Int)] -> Int -> Int
-digitFromSum sums n = snd (std !! 0)
-    where
-        std = filter (\p -> if (snd p) == n then True else False) sums
+        occs = map (\c -> length (filter (==c) (concat ss))) ['a'..'g']
 
 digitsToSum :: [Int] -> Int
 digitsToSum = read . concatMap show
 
+getNum :: Map Char Int -> String -> Int
+getNum m s = stdMap ! sum (map (m !) s)
+
+solveCombo :: Combo -> Int
+solveCombo (ds,ns) = digitsToSum (map (getNum (makeMap ds)) ns)
+
 part2 :: [Combo] -> Int
-part2 cs = sum $ map cs' cs
-    where
-        cs' xs = digitsToSum $ map digitFromSum (map (sumFromStr) (snd xs))
-        
+part2 cs = sum $ map solveCombo cs
