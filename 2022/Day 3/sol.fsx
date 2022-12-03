48d02ca94d3b6e1bf6336563d1a1d04327dc673b
Victor Olin <victor.vieweg@icloud.com>
2022-12-03 11:35:22 +0100

    clean up

--- 2022/Day 3/sol.fsx
+++ 2022/Day 3/sol.fsx
@@ -1,36 +1,28 @@
 let splitEq (s: string) =
-    let a = s.[0..(s.Length/2)-1] |> Seq.toList // |> List.map int
-    let b = s.[(s.Length/2)..] |> Seq.toList // |> List.map int
+    let a = s.[0..(s.Length/2)-1] |> Set.ofSeq // |> List.map int
+    let b = s.[(s.Length/2)..] |> Set.ofSeq // |> List.map int
     (a, b)
 
 let dict =
     (List.zip ['a'..'z'] [1..26]) @ (List.zip ['A'..'Z'] [27..52])
     |> Map.ofList
 
-let intersect (a: list<char>) (b: list<char>) =
-    a |> List.filter (fun x -> List.contains x b)
-
-let intersect2 (a: list<char>) (b: list<char>) (c: list<char>) =
-    a |> List.filter (fun x -> List.contains x b && List.contains x c)
-
 let input = System.IO.File.ReadAllLines "input.txt"
             |> Array.toList
 
-let score = List.map (Seq.distinct >> List.ofSeq)
-            >> List.map (fun x -> x |> List.map (fun y -> dict.[y]))
-            >> List.map (fun x -> x |> List.sum)
-            >> List.sum
+let score = Seq.map (Seq.distinct >> Set.ofSeq)
+            >> Seq.map (fun x -> x |> Set.map (fun y -> dict.[y]))
+            >> Seq.map (fun x -> x |> Set.toList |> List.sum)
+            >> Seq.sum
 
-let part1 = input
-            |> List.map splitEq
-            |> List.map (fun (a, b) ->(intersect a b))
-            |> score
-            |> printfn "Part 1: %d"
+input   |> List.map (splitEq)
+        |> Seq.ofList
+        |> Seq.map (fun (a, b) -> Set.intersect a b)
+        |> score
+        |> printfn "Part 1: %d"
 
-let part2 = input
-            |> List.map (Seq.toList)
-            |> (Seq.chunkBySize 3 >> Seq.toList)
-            |> List.map (Array.toList >> List.ofSeq)
-            |> List.map (fun x -> intersect2 (x.Item 0) (x.Item 1) (x.Item 2) )
-            |> score
-            |> printfn "Part 2: %d"
+input   |> List.map (Set.ofSeq)
+        |> Seq.chunkBySize 3
+        |> Seq.map (fun x -> Set.intersectMany x)
+        |> score
+        |> printfn "Part 2: %d"
