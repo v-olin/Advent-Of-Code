159f7387a154a5c372c61b6f0f82dcfcdfc83a61
Victor Olin <victor.vieweg@icloud.com>
2022-12-08 19:23:07 +0100

    golfing :DDD ;((

--- 2022/Day 8/sol.fsx
+++ 2022/Day 8/sol.fsx
@@ -1,49 +1,38 @@
-let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-
-let inline charToInt c = (int c - int '0')
-
 let zipper (a: List<'T>) (b: List<'T>) =
     let zipper'  (b: List<'T>) (a: 'T) = List.map (fun x -> (a, x)) b
     List.map (zipper' b) a |> List.concat
 
-let checkRow (m: List<List<int>>) (x: int) (y: int) =
-    let row = m.[y]
+let checkVec (m: List<List<int>>) (x: int) (y: int) =
+    let vec = m.[y]
     let me = m.[y].[x]
-    let left = List.take x row |> List.map (fun x -> x < me) |> List.fold (&&) true
-    let right = List.skip (x+1) row |> List.map (fun x -> x < me) |> List.fold (&&) true
-    left || right
-
-let checkCol (m: List<List<int>>) (x: int) (y: int) =
-    let col = (List.transpose m).[x]
-    let me = m.[y].[x]
-    let up = List.take y col |> List.map (fun x -> x < me) |> List.fold (&&) true
-    let down = List.skip (y+1) col |> List.map (fun x -> x < me) |> List.fold (&&) true
-    up || down
+    let a = List.take x vec |> List.map (fun x -> x < me) |> List.fold (&&) true
+    let b = List.skip (x+1) vec |> List.map (fun x -> x < me) |> List.fold (&&) true
+    a || b
 
 let checkPos (m: List<List<int>>) =
-    let xs = [1..(m.[0].Length)-2]
-    let ys = [1..m.Length-2]
-    let xys = zipper xs ys
-    let horizontal = List.map (fun t -> checkRow m (fst t) (snd t)) xys
-    let vertical = List.map (fun t -> checkCol m (fst t) (snd t)) xys
-    let zipped = List.zip horizontal vertical
-    let inner = List.map (fun t -> if (fst t) || (snd t) then 1 else 0) zipped |> List.sum
-    let outer = (m.Length)*2 + (m.[0].Length)*2 - 4
-    outer + inner
+    let xys = zipper [1..(m.[0].Length)-2] [1..m.Length-2]
+    let hs = List.map (fun t -> checkVec m (fst t) (snd t)) xys
+    let vs = List.map (fun t -> checkVec (List.transpose m) (snd t) (fst t)) xys
+    let zs = List.zip hs vs
+    let is = List.map (fun t -> if (fst t) || (snd t) then 1 else 0) zs |> List.sum
+    is + (m.Length)*2 + (m.[0].Length)*2 - 4
 
 let rec foldWhile' (s: int) (h: int) (l: List<int>)  =
     if l.Length = 0 then s
     elif l.Head < h then (foldWhile' (s+1) h l.Tail)
     else s+1
 
+let views (m: List<List<int>>) (x: int) (y: int) (me: int) =
+    let a = List.take x m.[y] |> List.rev |> foldWhile' 0 me
+    let b = List.skip (x+1) m.[y] |> foldWhile' 0 me
+    (a,b)
+
 let viewDistance (m: List<List<int>>) (x: int) (y: int) =
-    let me = m.[y].[x]
-    let left = List.take x m.[y] |> List.rev |> foldWhile' 0 me
-    let right = List.skip (x+1) m.[y] |> foldWhile' 0 me
     let m' = List.transpose m
-    let up = List.take y m'.[x] |> List.rev |> foldWhile' 0 me
-    let down = List.skip (y+1) m'.[x] |> foldWhile' 0 me
-    left * right * up * down
+    let me = m.[y].[x]
+    let (l, r)  = views m x y me
+    let (u, d) = views (List.transpose m) y x me
+    l * r * u * d
 
 let findMaxView (m: List<List<int>>) =
     let xs = [1..(m.[0].Length)-2]
@@ -51,12 +40,10 @@ let findMaxView (m: List<List<int>>) =
     let xys = zipper xs ys
     List.map (fun t -> viewDistance m (fst t) (snd t)) xys |> List.max
 
+let inline charToInt (c: char) = (int c - int '0')
+
 let input = System.IO.File.ReadAllLines "input.txt" |> Array.toList
 let parsed = List.map (Seq.toList >> List.map charToInt) input
 
 printfn $"Part 1: {checkPos parsed}"
 printfn $"Part 2: {findMaxView parsed}"
-
-stopWatch.Stop()
-printfn "Execution time: %.2f ms" stopWatch.Elapsed.TotalMilliseconds
-// Execution time: 4988.68 ms
